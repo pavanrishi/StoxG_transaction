9784d7b567cfee53f9b6f0ae38f400f83759d43e
Devarasetty pavan rishi <90823777+pavanrishi@users.noreply.github.com>
2023-07-19 01:29:37 +0530

    Delete src directory

--- src/Components/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-export default function HomePage () {
-    return (
-	<div>
-		<Link to='/Transcation'><div>Transcation</div></Link>
-	</div>
-	)
-}
